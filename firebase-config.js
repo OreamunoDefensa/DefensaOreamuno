4b13c0662a7fe606e1bb6de9420549aa3d4a7b5d
OreamunoDefensa <defensaoreamuno@gmail.com>
2025-05-09 14:33:39 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBbns8BxRr7QnIdqlDQ9n9HPaDQ7xGa2s",
+  authDomain: "defensaoreamuno-b1750.firebaseapp.com",
+  projectId: "defensaoreamuno-b1750",
+  storageBucket: "defensaoreamuno-b1750.firebasestorage.app",
+  messagingSenderId: "891926657601",
+  appId: "1:891926657601:web:59f3dda70e70d9ed3b64c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
